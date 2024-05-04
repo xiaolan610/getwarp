9e8fb859f136d8c72d393deaeb6cf90367f1ee0b
lan <lan_at@163.com>
2024-05-04 20:48:09 +0800

    Update warp.py

--- warp.py
+++ warp.py
@@ -9,7 +9,7 @@ import sys
 os.system("title WARP-PLUS-CLOUDFLARE By ALIILAPRO")
 os.system('cls' if os.name == 'nt' else 'clear')
 print('Getting WARP+ Traffic on Github Actions')
-referrer = os.environ["DEVICEID"]
+referrer = "c8c65ab5-2c9f-42de-ad94-64ab73531cbe"
 def genString(stringLength):
 	try:
 		letters = string.ascii_letters + string.digits
